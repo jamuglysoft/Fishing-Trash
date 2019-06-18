5898874be0c666ab2ff29948009e4b815c001c4d
VictorSegura99 <36154682+VictorSegura99@users.noreply.github.com>
2019-06-19 00:14:16 +0200

    Estoy ultra deprimido

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -186,6 +187,11 @@ public class PlayerController : MonoBehaviour
             yield return null;
         }
         grow = false;
+    }
+
+    private void PlayerDead()
+    {
+        SceneManager.LoadScene(("MainMenu"), LoadSceneMode.Single);
     }
 
 }

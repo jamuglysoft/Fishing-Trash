55b0df2f5194003f9d33ce19b98314fdbbe25830
VictorSegura99 <36154682+VictorSegura99@users.noreply.github.com>
2019-06-18 22:38:14 +0200

    Now sounds work again jeje :)

---
+++ Assets/Scripts/BubblesOver.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubblesOver : MonoBehaviour
+{
+    public GameObject playBubble;
+    private GameObject buble;
+
+    public void SetBubble()
+    {
+        buble = Instantiate(playBubble);
+    }
+
+    public void DestroyBubble()
+    {
+        Destroy(buble);
+    }
+}

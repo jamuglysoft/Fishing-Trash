839afe782df5801dc5b0baa48ea8e7cbb3acbc52
Christian Martínez de la Rosa <36189169+christt105@users.noreply.github.com>
2019-06-18 11:07:29 +0200

    Parallax

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private Vector3 init_position;
+    public Camera cam;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        init_position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = cam.transform.position * speed + init_position;
+    }
+}

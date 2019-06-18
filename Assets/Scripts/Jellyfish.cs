f3fcd7f37c8526485d4a5b574fb6573a33cc8606
Christian Martínez de la Rosa <36189169+christt105@users.noreply.github.com>
2019-06-18 14:23:34 +0200

    Jellyfish script

---
+++ Assets/Scripts/Jellyfish.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jellyfish : MonoBehaviour
+{
+    public float max_up;
+    public float min_up;
+    private bool go_up = true;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (go_up)
+        {
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
+            if (transform.position.y >= max_up)
+                go_up = !go_up;
+        }
+        else
+        {
+            transform.Translate(Vector3.down * speed * Time.deltaTime);
+            if (transform.position.y <= min_up)
+                go_up = !go_up;
+        }
+    }
+}

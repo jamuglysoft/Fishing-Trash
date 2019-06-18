7c5825a57548ca163a7db05ad39971a462cb132f
VictorSegura99 <36154682+VictorSegura99@users.noreply.github.com>
2019-06-18 20:32:40 +0200

    Audio Manager

---
+++ Assets/Scripts/AudioScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+
+    static AudioSource audioSrc;
+
+    public static AudioClip laser_sfx;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSrc = GetComponent<AudioSource>();
+
+        laser_sfx = Resources.Load<AudioClip>("Laser");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void PlaySound(string clip)
+    {
+        switch(clip)
+        {
+            case "laser":
+                audioSrc.PlayOneShot(laser_sfx);
+                break;
+        }
+    }
+
+}

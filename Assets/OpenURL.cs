aef7b51c4bbd570b6eaa6682bf5c5c3ef22a533e
youis11 <lluis.moreu@gmail.com>
2019-06-19 09:23:00 +0200

    Credits menu done

---
+++ Assets/OpenURL.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenURL : MonoBehaviour
+{
+    public void Oriol ()
+    {
+        Application.OpenURL("https://github.com/OriolCS2");
+    }
+
+    public void Marc()
+    {
+        Application.OpenURL("https://github.com/optus23");
+    }
+
+    public void Christian ()
+    {
+        Application.OpenURL("https://github.com/christt105");
+    }
+
+    public void Lluis ()
+    {
+        Application.OpenURL("https://github.com/youis11");
+    }
+
+    public void Victor ()
+    {
+        Application.OpenURL("https://github.com/VictorSegura99");
+    }
+}
